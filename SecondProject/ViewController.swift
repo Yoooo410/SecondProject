0c4b1b22e3179b425cb5390539cf8a32d5fa9f4d
Yoooo410 <yoshitokomiya@ccl107im04.local>
2017-05-24 14:18:15 -0700

    Initial Commit

---
+++ SecondProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SecondProject
+//
+//  Created by yoshitokomiya on 2017-05-24.
+//  Copyright © 2017 yoshitokomiya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
